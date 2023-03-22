c1a27d7a12f5dbece6022ce06c4d76d31fb312c1
Otavio Fina <otaviofina@gmail.com>
2023-03-22 16:35:42 -0300

    vr 1.3.2
    PAGINA INICIAL COMPLETA. com os btn acima da img dos projetos implementada

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,22 @@ const segundoProjetoDestaque = document.querySelector('#segundo-projeto-destaque
 const primeiroPDestaque = document.querySelector('#primeiro-p-destaque');
 const segundoPDestaque = document.querySelector('#segundo-p-destaque');
 
+const primeiroH3Destaque = document.querySelector('#primeiro-h3-destaque');
+const segundoH3Destaque = document.querySelector('#segundo-h3-destaque');
+
+const primeiroProjetoBtn = document.querySelector("#primeiro-projeto-btn");
+const segundoProjetoBtn = document.querySelector('#segundo-projeto-btn');
+
 
 
 
 function MudancaDeBackground(e ,nome) {
-    e.style.backgroundImage = 'url(../public/iamges/foto-projeto/"' + nome + '")';
+    e.style.backgroundImage = 'url(../public/images/foto-projeto/' + nome + ')';
     e.style.backgroundSize = 'contain';
 }
 
-function MudancaDeDescricao(e ,descricao) {
-    e.innerHTML = descricao
+function MudancaDeTexto(e ,texto) {
+    e.innerHTML = texto
 }
 
 function RedirecionamentoClick(e ,url) {
@@ -34,11 +40,15 @@ for ( let x in meusProjetos2.projetos) {
     let a = meusProjetos2.projetos
     if (a[x].destaque1) {
         MudancaDeBackground(primeiroProjetoDestaque, a[x].imagemNome)
-        MudancaDeDescricao(primeiroPDestaque, a[x].descricao)
+        MudancaDeTexto(primeiroPDestaque, a[x].descricao)
+        MudancaDeTexto(primeiroH3Destaque, a[x].nome)
         RedirecionamentoClick(primeiroProjetoDestaque, a[x].link)
+        RedirecionamentoClick(primeiroProjetoBtn, a[x].link)
     } else if (a[x].destaque2) {
         MudancaDeBackground(segundoProjetoDestaque, a[x].imagemNome)
-        MudancaDeDescricao(segundoPDestaque, a[x].descricao)
+        MudancaDeTexto(segundoPDestaque, a[x].descricao)
+        MudancaDeTexto(segundoH3Destaque, a[x].nome)
         RedirecionamentoClick(segundoProjetoDestaque, a[x].link)
+        RedirecionamentoClick(segundoProjetoBtn, a[x].link)
     }
 }
